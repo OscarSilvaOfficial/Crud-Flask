6a6791a89a630bf992c79414e97b12b174a53721
DinoraVicente <dondi8181@gmail.com>
2020-10-25 18:41:57 -0300

    Migração de arquitetura para adição do Nuxt js

--- backend/entities.py
+++ backend/entities.py
@@ -3,3 +3,9 @@ class Jogo:
         self.jogo = jogo
         self.categoria = categoria
         self.console = console
+
+class Usuario:
+    def __init__(self, id, nome, senha):
+        self.id = id
+        self.nome = nome
+        self.senha = senha
---
+++ backend/routers.py
@@ -0,0 +1,70 @@
+from flask import *
+from backend.entities import *
+
+app = Flask(__name__, template_folder="../frontend/templates", static_folder='../frontend/static')
+lista = []
+
+usuario = Usuario('admin', 'Oscar da Silva', 'admin')
+usuario2 = Usuario('teste', 'Meu Teste', 'teste')
+
+usuarios = {
+    usuario.id: usuario,
+    usuario2.id: usuario2
+}
+
+@app.route('/')
+def index():
+    if 'usuario_logado' not in session:
+        return redirect('/login')
+    else:
+        user = usuarios[session['usuario_logado']].nome
+        return render_template('components/index.html', titulo='Jogos', lista=lista, user=user)
+
+@app.route('/novo')
+def novo():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('login', proxima=url_for('novo')))
+    else:
+        user = usuarios[session['usuario_logado']].nome
+        return render_template('components/create.html', titulo='Novo Jogo', user=user)
+
+@app.route('/create', methods=['POST',])
+def create():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo) 
+    return redirect(url_for('index'))
+
+@app.route('/login')
+def login():
+    proxima = request.args.get('proxima')
+    return render_template('components/auth/login.html', proxima=proxima)
+
+@app.route('/autenticar', methods=['POST',])
+def autenticar():
+    if request.form['usuario'] in usuarios:
+        usuario = usuarios[request.form['usuario']]
+        if usuario.senha == request.form['senha']:
+            session['usuario_logado'] = usuario.id
+            flash(f'{usuario.nome.upper()} Está logado!')
+            proxima_pagina =  request.form['proxima']
+            if proxima_pagina != 'vazio':
+                return redirect(proxima_pagina)   
+            else:
+                return redirect(url_for('index'))
+        else:
+            flash('Credenciais incorretas! Insira as credenciáis corretas.')
+            return redirect(url_for('login'))
+    else:
+        flash('Credenciais incorretas! Insira as credenciáis corretas.')
+        return redirect(url_for('login'))
+@app.route('/logout')
+def logout():
+    if session.get('usuario_logado'):
+        session.clear()
+        flash('Usuário deslogado!')
+        return redirect(url_for('login'))
+    else:
+        return redirect(url_for('login'))
