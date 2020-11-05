9f6eab38dea185435059c75d12771af69d37fb44
Oscar da Silva <oscarkaka222@gmail.com>
2020-11-04 21:11:19 -0300

    Alterando config para funcionar no heroku

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from backend import routers
+import os
 
 app = routers.app
 app.secret_key = "asdokifhsAFHJQOIURHWEF'12983791283RQIGFE" 
-app.run(debug=True)
-
-
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
