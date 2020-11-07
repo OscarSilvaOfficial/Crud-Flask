e9c48fbb7ee44b4b1e11201cfa00c78bf1833e21
Oscar da Silva <oscarkaka222@gmail.com>
2020-11-06 23:11:12 -0300

    voltando para o mysql

--- backend/ext/configuration.py
+++ backend/ext/configuration.py
@@ -1,8 +1,12 @@
 from flask_mysqldb import MySQL
 from backend.dao import *
 from flask import *
+import sqlite3
+from flask import g
 
 app = Flask(__name__, template_folder="../../frontend/templates", static_folder='../../frontend/static')
 db = MySQL(app)
 jogo_dao = JogoDao(db)
 usuario_dao = UsuarioDao(db)
+
+DATABASE = 'C:\\Users\\oscar\\OneDrive\\Área de Trabalho\\Dev\\Crud-Flask\\flaskr.sqlite'
--- run.py
+++ run.py
@@ -1,14 +1,17 @@
 from backend.ext.configuration import *
 from backend.auth import routers
 from backend.jogo import routers
+import os
 
+app.config.from_object(__name__) # load config from this file
 app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'jogoteca'
 app.config['MYSQL_port'] = '3306'
 
-app.secret_key = "asdokifhsAFHJQOIURHWEF'12983791283RQIGFE" 
+app.secret_key = "asdokifhsAFHJQOIURHWEF12983791283RQIGFE" 
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
